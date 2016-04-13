5325f4ed280a465a34ea557baa9d0c4502b52eba
Michael Angell <m.angell@culturekings.com.au>
2016-04-13 10:32:40 +1000

    Handle web hook events by sending 200 response

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.get('/webhook', function (req, res) {
     }
 });
 
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            // Handle a text message from this sender
+        }
+    }
+    res.sendStatus(200);
+});
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
