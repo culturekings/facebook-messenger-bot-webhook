4afddf32443d99286fb5deaf8daa6c60eac702d6
Michael Angell <m.angell@culturekings.com.au>
2016-04-14 15:35:17 +1000

    Turn in to Echo bot by default

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Facebook Messenger Bot Skeleton
 Skeleten Application to handle the authentication and webhook requests sent via facebook for new messenger bots.
 
-This project is developed to work with Heroku with no coding changes required. Obviously the Bot won't do anything unless you add some of your own logic.
+This project is developed to work with Heroku with no coding changes required. It implements the standard echo function that is provided by Facebook to provide visual feedback it is working.
 
 This project is written in Node.js and Express to stay in line with the official Facebook docs and make it easier to use their provided examples.
 
 It has a default Verify Token of `TEST` however this can be changed be setting an environmental variable in heroku of `VERIFY_TOKEN`
+
+There is also a requirement for a `PAGE_ACCESS_TOKEN` which is provided during the Facebook setup below. If you use the Heroku Button it will ask you for this during setup.
 
 To see full instructions on how to enable in Facebook. [Follow the official guide](https://developers.facebook.com/docs/messenger-platform/quickstart)
 
--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('verify_token', (process.env.VERIFY_TOKEN || 'TEST'));
+app.set('page_access_token', (process.env.PAGE_ACCESS_TOKEN || 'NULL'));
+
+app.get('/', function (req, res) {
+        res.send('It Works! Follow FB Instructions to activate.');
+});
 
 app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === app.get('verify_token')) {
@@ -19,11 +24,33 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id;
         if (event.message && event.message.text) {
             text = event.message.text;
-            // Handle a text message from this sender
+            // Your Logic Replaces the following Line
+            sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
         }
     }
     res.sendStatus(200);
 });
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:app.get('page_access_token')},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
