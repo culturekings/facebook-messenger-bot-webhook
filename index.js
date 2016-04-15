1a10c76de708173b43cdac05abb4c17b542685c4
Michael Angell <m.angell@culturekings.com.au>
2016-04-15 18:14:43 +1000

    log req body

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.get('/webhook', function (req, res) {
 });
 
 app.post('/webhook/', function (req, res) {
+    console.log (req.body);
     messaging_events = req.body.entry[0].messaging;
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i];
