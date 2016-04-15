3038071b11fd6d21192b6e7a62715ae301d31008
Michael Angell <m.angell@culturekings.com.au>
2016-04-15 18:25:34 +1000

    Add missing request dependency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var request = require('request');
 
 app.use(bodyParser.json());
 
